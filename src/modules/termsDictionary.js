9a0bac60b23e66540fe14e0db24ac9b1945f5a7c
Javier <jaxolotl@gmail.com>
2018-10-16 17:46:46 -0300

    - Introduce SoC - Refactor / Rename Authentication and Requestor classes - removes all non-auth related code ( as much as possible )

---
+++ src/modules/termsDictionary.js
@@ -0,0 +1,17 @@
+
+const TERMS_DICTIONARY = {
+    'CONNECTOR_NAME': 'Spotify Connector',
+    'ERROR': {
+        'SAVE_TOKENS_TO_PASSWD': 'Authentication.saveTokensToPassword value must contain access_token and refresh_token',
+        'MISSING_AUTH': 'Missing Authentication!',
+        'UNABLE_TO_REFRESH_TOKENS': 'Unable to refresh tokens',
+        'DEFAULT_ERROR': 'An unrecoverable error occurred. Contact Support Team'
+    },
+    'TIME_RANGE': {
+        'SHORT_TERM': 'Last 4 weeks aprox. of data.',
+        'MEDIUM_TERM': 'Last 6 months aprox. of data.',
+        'LONG_TERM': 'Several years of data.'
+    }
+};
+
+export default TERMS_DICTIONARY;
