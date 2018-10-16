96682a4d01b1886f4491611ea0351c10d1d402b9
Javier <jaxolotl@gmail.com>
2018-10-16 18:19:38 -0300

    - retrieve schema

--- src/modules/Requestor.js
+++ src/modules/Requestor.js
@@ -1,2 +1,24 @@
 
+import ADVANCED_SCHEMA from '../schemas/advancedSchemas';
 export const DEFAULT_TIME_RANGE = 'short_term';
+/**
+ * Requestor
+ * 
+ * This class abstracts away most of the interaction with Spotify's API.
+ * All methods return promises which will be resolved once the requested resource has been returned from Spotify
+ */
+class Requestor {
+
+    /**
+     * 
+     * @returns {Object} Promise/A+
+     */
+    retrieveSchema () {
+        /**
+         * We use static schema but want to leave the door open for an async schema retrieval
+         */
+        return Promise.resolve(ADVANCED_SCHEMA);
+    }
+}
+
+export default Requestor;
